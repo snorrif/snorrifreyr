72263ee8a4f148a51b7bf1142ce344c69f8936cb
snorri <snorrifreyr07@gmail.com>
2025-10-08 04:07:57 +0000

    hit

--- snorrifreyr/src/components/sections/blackjack.jsx
+++ snorrifreyr/src/components/sections/blackjack.jsx
@@ -1,6 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
-  const [money,setmoney]=Int16Array;
+
   function randomIntFromInterval(min, max) { 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -11,6 +11,8 @@ const rndInt = randomIntFromInterval(1, 13);
 
 export const Blackjack =()=>{
   const [cards,setcards] =useState([]);
+  const [money,setmoney]=useState([]);
+  const handlehit= () => {setcards(rndInt(1,13))};
 
     return <section id="Blackjack" className="min-h-screen bg-[#0a0a0b] text-zinc-200 selection:bg-lime-400/30 selection:text-zinc-50">
         con
@@ -21,7 +23,7 @@ export const Blackjack =()=>{
     <h4>money=[money]</h4>
 
 
-    <input type="button" value="hit" onClick={setcards(randomIntFromInterval(1, 13)) }/>
+    <input type="button" value="hit" onClick={handlehit }/>
     <button>stand</button>
     
     =
